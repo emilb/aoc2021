6a6ebcf7155102b236ca7349976afe519462b9bc
Emil Breding <emil.breding@gmail.com>
2021-11-30 22:22:49 +0100

    Fixed formatting

--- template/index.js
+++ template/index.js
@@ -1,24 +1,21 @@
 const fs = require('fs')
 
-function inputDataLinesIntegers(filename="input.txt") {
-    return fs.readFileSync(filename).toString().trim().split("\n").map((x)=>parseInt(x))
+function inputDataLinesIntegers (filename = 'input.txt') {
+  return fs.readFileSync(filename).toString().trim().split('\n').map((x) => parseInt(x))
 }
 
-function getSolutionPart1() {
-    return inputDataLinesIntegers().reduce((x,y)=>x+y)
+function getSolutionPart1 () {
+  return inputDataLinesIntegers().reduce((x, y) => x + y)
 }
 
-function getSolutionPart2() {
-    return inputDataLinesIntegers().reduce((x,y)=>x*y)
+function getSolutionPart2 () {
+  return inputDataLinesIntegers().reduce((x, y) => x * y)
 }
 
-const part = process.env.part || "part1"
+const part = process.env.part || 'part1'
 
-if (part === "part1")
-    console.log(getSolutionPart1())
-else
-    console.log(getSolutionPart2())
+if (part === 'part1') { console.log(getSolutionPart1()) } else { console.log(getSolutionPart2()) }
 
 module.exports = {
-    getSolutionPart1, getSolutionPart2, inputDataLinesIntegers
+  getSolutionPart1, getSolutionPart2, inputDataLinesIntegers
 }
--- template/test/index.test.js
+++ template/test/index.test.js
@@ -1,10 +1,10 @@
-const {getSolutionPart1, getSolutionPart2} = require('../index')
 
-test('part1', ()=>{
-    expect(getSolutionPart1()).toBe(1379)
+const { getSolutionPart1, getSolutionPart2 } = require('../index')
+
+test('part1', () => {
+  expect(getSolutionPart1()).toBe(1379)
 })
 
-test('part2', ()=>{
-    expect(getSolutionPart2()).toBe(56154)
+test('part2', () => {
+  expect(getSolutionPart2()).toBe(56154)
 })
-
