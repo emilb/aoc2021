29727276af872e25a9d939e3a87d2397e8562d94
Emil Breding <emil.breding@gmail.com>
2021-11-30 20:50:16 +0100

    Fixed template project and setting up scripts

---
+++ prepare-day.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+usage () { echo "Copies a template project to specified day
+
+HOW TO USE:
+$ prepare-day.sh <daynum>
+EXAMPLE:
+$ prepare-day.sh 06
+"; }
+
+if [[ $# -eq 0 ]]; then
+  usage; exit;
+fi
+
+echo "cp -r template/* day${1}/"
+cp -r template/* day${1}/
+cd "day${1}"
+
+old=${1}
+shortDay=$(echo $old | sed 's/^0*//')
+
+echo "Fetching problem input..."
+nog -d "${shortDay}" -y 2021 -i > input.txt
+
+echo "Fetching problem puzzle..."
+nog -d "${shortDay}" -y 2021 -p | sed '1,22d' > puzzle.ansi
